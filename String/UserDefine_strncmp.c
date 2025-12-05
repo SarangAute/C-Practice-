b46abe8ffaec70186d525ac37d34e638523cc569
SarangAute <sarangsaute@gmail.com>
2025-12-05 20:02:51 +0530

    Update UserDefine_strncmp.c

--- String/UserDefine_strncmp.c
+++ String/UserDefine_strncmp.c
@@ -1,18 +1,19 @@
+
 #include<stdio.h>
 int UDstrncmp(char* s1, char* s2, int n){
 	int i;
-	for(i=0; (s1[i] == s2[i])&& i<=n; i++){
-		if(s1[i] == '\0' || s2[i]== '\0' )
+	for(i=0; i<=n; i++){
+		if((s1[i] != s2[i]) || s1[i] == '\0' || s2[i]== '\0' )
 			break;
 	}
-	
+	--i;
 	return s1[i]-s2[i];
 }
 int main(){
 	char s1[40], s2[40];
 	int n;
 	printf("ENTER THE STRING 1 : \n");
-	scanf(" %[^\n]", s1);
+	scanf("%[^\n]", s1);
 
 	printf("ENTER THE STRING 2 : \n");
 	scanf(" %[^\n]", s2);
