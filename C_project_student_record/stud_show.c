a606a898cc9b45e04113df99c9f9756095556ade
Sarang Suresh Aute <sarangsaute@gmail.com>
2026-03-30 10:01:13 +0530

    Delete C_project_student_record directory

--- C_project_student_record/stud_show.c
+++
@@ -1,9 +0,0 @@
-#include"header.c"
-
-void stud_show(struct stud_data* ptr){
-        while(ptr != 0){
-                printf("%d %s %f\n",ptr->roll,ptr->name,ptr->marks);
-                ptr=ptr->next;
-        }
-}
-
