290002850eb9678b5ac10ed4699d2c1031dc4466
Sarang Suresh Aute <sarangsaute@gmail.com>
2026-04-10 09:52:24 +0530

    Update Bubble_Sort.c

--- Sorting_Technique/Bubble_Sort.c
+++ Sorting_Technique/Bubble_Sort.c
@@ -2,7 +2,7 @@
 int main(){
 	int a[5], temp, n = sizeof(a)/sizeof(int);
 
-	printf("ENTER THE NUMBERS IN ARRAY OF SIZE 5 : ")
+	printf("ENTER THE NUMBERS IN ARRAY OF SIZE 5 : ");
 	for(int i=0; i<n; i++){
 		printf("FOR INDEX %d : ",i);
 		scanf("%d",&a[i]);
